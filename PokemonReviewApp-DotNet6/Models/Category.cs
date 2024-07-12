92cda05f5245ddd79d295953ec23901249032ed5
aishyuu <ivanmatias1331@gmail.com>
2024-07-11 20:54:34 -0700

    Add project files.

---
+++ PokemonReviewApp-DotNet6/Models/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace PokemonReviewApp_DotNet6.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ PokemonReviewApp-DotNet6/Models/Owner.cs
@@ -0,0 +1,8 @@
+﻿namespace PokemonReviewApp_DotNet6.Models;
+
+public class Owner
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Gym { get; set; }
+}
